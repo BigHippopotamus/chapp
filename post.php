f6179e034b81fde16bdb896ec594b8609f7e46d4
Vidharshan <vidharsh2003@gmail.com>
2021-07-11 19:31:15 +0530

    Merge branch 'main' of https://github.com/BigHippopotamus/chapp

--- post.php
+++ post.php
@@ -2,5 +2,6 @@
 session_start();
 
 $message = "<p><span id='uname'>" . $_SESSION['user'] . "</span>: " . $_POST['message'] . "</p>\n";
+if (!file_exists($_POST['link'])) exit;
 file_put_contents($_POST['link'], $message, FILE_APPEND);
 ?>
