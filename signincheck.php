41ded6c154b890cd37307c55b793f53e3dc674f4
BigHippopotamus <thegrimreaper200@gmail.com>
2021-07-11 19:19:17 +0530

    Logging in and signing in work

--- signincheck.php
+++ signincheck.php
@@ -11,18 +11,18 @@
 
         $sqlu = "SELECT * FROM $usertable WHERE uname='$uname'";
         $sqle = "SELECT * FROM $usertable WHERE email='$email'";
-        $resu = mysqli_query($conn, $sqlu);// or die(mysqli_error($conn));
-        $rese = mysqli_query($conn, $sqle);// or die(mysqli_error($conn));
+        $resu = mysqli_query($conn, $sqlu);
+        $rese = mysqli_query($conn, $sqle);
 
-        if(mysqli_num_rows($resu)){
+        if(mysqli_num_rows($resu) > 0){
             $uerror = "This username has already been taken. Please go for another one.";
             $_SESSION['utaken'] = $uerror;
-        }else if(mysqli_num_rows($resu)){
-            echo "HI";
+            header("Location: index.php");
+        }else if(mysqli_num_rows($rese) > 0){
             $eerror = "An user has already signed-in with this Email ID. Please use an other Email ID or try to Log in.";
             $_SESSION['etaken'] = $eerror;
+            header("Location: index.php");
         }else{
-            echo "WELCOM";
             $sqlins = "INSERT INTO usertable (uname, pwd, email)
             VALUES ('$uname', '$pass', '$email')";
             if (mysqli_query($conn, $sqlins)) {
